c5387cff697660fb1493b21aa694e52923643089
andreaalarconvaldes <andreaalarconvaldes@gmail.com>
2025-06-12 18:37:17 +0100

    install typescript

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer class="footer">
+    <footer className="footer">
       <div className="footer-info">
-        <div class="footer-section about">
+        <div className="footer-section about">
           <div className="footer-logo-title">
             <img src="./logo.svg" alt="" />
             <h3>Artisan Bakery</h3>
@@ -20,7 +20,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div class="footer-section links">
+        <div className="footer-section links">
           <h3>Quick Links</h3>
           <ul>
             <li>
@@ -38,7 +38,7 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div class="footer-section contact">
+        <div className="footer-section contact">
           <h3>Contact Info</h3>
           <div className="contact-info">
             <img src="./location.svg" alt="" />
@@ -57,14 +57,14 @@ const Footer = () => {
           </div>
         </div>
 
-        <div class="footer-section hours">
+        <div className="footer-section hours">
           <h3>Store Hours</h3>
           <p>Monday - Friday: 6:00 AM - 8:00 PM</p>
           <p>Saturday: 7:00 AM - 9:00 PM</p>
           <p>Sunday: 8:00 AM - 6:00 PM</p>
         </div>
       </div>
-      <div class="footer-bottom">
+      <div className="footer-bottom">
         &copy; {new Date().getFullYear()} Artisan Bakery – Portfolio project by
         Andrea Alarcón Valdés.
       </div>
